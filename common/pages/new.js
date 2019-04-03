fe14b941a434e87306a3b25248da49963684426f
Rob Chandler <robkadt@gmail.com>
2019-04-03 22:32:15 +0100

    Adds prefetch to newest page

--- common/pages/new.js
+++ common/pages/new.js
@@ -1,8 +1,9 @@
+import { apiFetch, prefetch } from "../fetch";
+import { nextPage, render } from "../utils";
+
 import Html from "../components/Html";
 import NewsList from "../components/NewsList";
-import { apiFetch } from "../fetch";
 import { h } from "preact";
-import { render } from "../utils";
 /** @jsx h */
 
 const Page = ({ news, page }) => (
@@ -12,6 +13,15 @@ const Page = ({ news, page }) => (
 export default async function renderPage(params, { page = 1 }) {
   const requestUrl = `https://api.hackerwebapp.com/newest?page=${page}`;
   const { data, isOffline } = await apiFetch(requestUrl);
+
+  const prefetchUrls = [
+    `https://api.hackerwebapp.com/newest?page=${nextPage(page)}`,
+    `https://api.hackerwebapp.com/new?page=1`,
+    `https://api.hackerwebapp.com/show?page=1`,
+    `https://api.hackerwebapp.com/ask?page=1`
+  ];
+
+  prefetchUrls.map(url => prefetch(url));
   return render(
     <Html title="Newest" offline={isOffline}>
       <Page news={data} page={page} />
