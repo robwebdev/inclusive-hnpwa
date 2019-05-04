f45c8c84ad66708cc69988118fc14dcdac756520
Rob Chandler <robkadt@gmail.com>
2019-05-04 14:04:22 +0100

    Fixes heading word break layout issue

--- client/service-worker.js
+++ client/service-worker.js
@@ -4,7 +4,7 @@ import { LONG_LIVED_OFFLINE_BACK_CACHE } from "../common/utils";
 import app from "../common/app";
 import routeMatcher from "route-matcher";
 
-const SWVERSION = "v0.2.12";
+const SWVERSION = "v0.2.13";
 const navigationHandler = handleNavigationRequest(app, {
   serviceWorkerVersion: SWVERSION
 });
